ba3988299c41063a913491042b6c201f97bab034
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-04-10 17:09:58 -0300

    install and configure lib stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,33 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  createTheme,
+  css,
+  styled,
+  theme,
+  getCssText,
+  keyframes,
+  globalCss,
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#fff',
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray400: '#8D8D99',
+
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
+    },
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+  },
+})
